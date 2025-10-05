e65013f4458ec9660b4e680dba153817627bd54e
Rodinez <mngtandrade@gmail.com>
2025-10-05 14:34:31 -0300

    eea e square

---
+++ EEA.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void eea(long long a, long long b, long long *mdc, long long *x, long long *y) {
+    long long s0 = 1, s1 = 0; 
+    long long t0 = 0, t1 = 1;
+    long long r0 = b, r1 = a;
+
+    long long q, r_temp, s_temp, t_temp;
+    while (r1 != 0) {
+        q = r0 / r1;
+
+        r_temp = r1;
+        r1 = r0 - q * r1;
+        r0 = r_temp;
+        
+        s_temp = s1;
+        s1 = s0 - q * s1;
+        s0 = s_temp;
+        
+        t_temp = t1;
+        t1 = t0 - q * t1;
+        t0 = t_temp;
+    }
+    
+    *mdc = r0;
+    *x = s0;
+    *y = t0;
+}
+
+int main() {
+    long long x, n;
+    scanf("%lld %lld", &x, &n);
+    long long mdc = 0, inverso = 0;
+    eea(x, n, &mdc, &inverso, &inverso);
+    
+    if (mdc == 1) {
+        // Ajusta o inverso para ser positivo
+        inverso = (inverso % n + n) % n;
+        printf("%lld %lld\n", mdc, inverso);
+    } else {
+        printf("%lld N\n", mdc);
+    }
+    
+    return 0;
+}
---
+++ square_and_mult.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long long square_and_mult(long long base, long long expoente, long long modulo) {
+    long long result = 1;
+    base = base % modulo;
+    
+    while (expoente > 0) {
+        if (expoente & 1)
+            result = (result * base) % modulo;
+        base = (base * base) % modulo;
+        expoente >>= 1;
+    }
+    
+    return result;
+}
+
+int main() {
+    long long x, n, k;
+    scanf("%lld %lld %lld", &x, &k, &n);
+    printf("%lld\n", square_and_mult(x, k, n));
+    
+    return 0;
+}
